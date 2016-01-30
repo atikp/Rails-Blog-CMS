c35599b6d6596c19e12f5585acd16b8f4383f034
atikp <atikpatel@hotmail.co.uk>
2016-01-30 18:24:11 +0000

    delete fix for pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ActiveRecord::Base
   belongs_to :category
   belongs_to :type
   
-  has_many :fields
+  has_many :fields, dependent: :destroy
   
   accepts_nested_attributes_for :fields, reject_if: :all_blank
   
